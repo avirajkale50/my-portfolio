74073905a82b46335505a52f311f172f66a0bd06
avirajkale50 <avirajkale2004@gmail.com>
2025-02-16 13:49:44 +0530

    header and about section added

---
+++ app/components/Header.jsx
@@ -0,0 +1,48 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+      <div>
+        <Image
+          src={assets.profile_img}
+          alt="profile"
+          className="rounded-full w-32 "
+        />
+      </div>
+      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+        Hi , I'm Aviraj Kale
+        <Image src={assets.hand_icon} alt="hand icon" className="w-6" />
+      </h3>
+      <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
+        Software Engineer Based in India.
+      </h1>
+      <p className="max-w-2xl mx-auto font-Ovo">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum deserunt
+        facere esse provident ab inventore? Molestias culpa voluptatum nesciunt
+        dicta.
+      </p>
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        <a
+          href="#contact"
+          className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2"
+        >
+          Contact Me
+          <Image src={assets.right_arrow_white} alt="icon" className="w-4" />
+        </a>
+        <a
+          href="/sample-resume.pdf"
+          download
+          className="px-10 py-3 border rounded-full border-grey-500 flex items-center gap-2"
+        >
+          My resume
+          <Image src={assets.download_icon} alt="icon" className="w-4" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
